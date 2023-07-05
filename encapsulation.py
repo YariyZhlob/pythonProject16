38b80528bf7819d2a06d2904cf5b0a701fff5e6a
yuriyknyazev <yurets.knyazev@mail.ru>
2023-07-06 03:29:25 +0500

    homework done

--- encapsulation.py
+++ encapsulation.py
@@ -14,3 +14,10 @@ class StartCar:
 start = StartCar()
 start.turn_key()
 start._switch_door_blocker()
+
+a = 90 +3j
+print(type(a))
+s = float('inf')
+print(type(s))
+x = False + False + False + True
+print(x)
